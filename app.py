56b73b09b7d0d9f1d6294ae93311ae43ab1dd553
dennisnderitu254 <dknderitu@gmail.com>
2023-09-18 16:44:49 +0300

    Linking templates using Flask url_for

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app = Flask(__name__, template_folder='templates')
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 @app.route('/home')
-def index():
+def home():
     return render_template('base.html')
 
 
