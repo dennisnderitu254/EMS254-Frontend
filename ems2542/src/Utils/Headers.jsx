e3efa4e9aafa1024e45eca1489a8bfa47221dd86
Bradkibs <Bradleykibs@gmail.com>
2023-09-24 19:34:35 +0300

    updating the login functionality

--- ems2542/src/Utils/Headers.jsx
+++ ems2542/src/Utils/Headers.jsx
@@ -1,6 +1,6 @@
 class ApiService {
   constructor() {
-    this.baseUrl = '0.0.0.0:5000';
+    this.baseUrl = 'http://127.0.0.1:5000/api/v1/views';
   }
 
   async sendAuthorizedRequest(endpoint, method = 'GET', data = null) {
@@ -17,6 +17,7 @@ class ApiService {
       body: data ? JSON.stringify(data) : null,
     };
 
+
     try {
       const response = await fetch(`${this.baseUrl}/${endpoint}`, requestOptions);
 
@@ -29,7 +30,33 @@ class ApiService {
     } catch (error) {
       throw error;
     }
-  }
+  };
+
+  async sendUnAuthorizedRequest(endpoint, method = 'GET', data = null) {
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    const requestOptions = {
+      method: method,
+      headers: headers,
+      body: data ? JSON.stringify(data) : null,
+    };
+
+
+    try {
+      const response = await fetch(`${this.baseUrl}/${endpoint}`, requestOptions);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      throw error;
+    }
+    }
 }
 
 export default new ApiService();
--- ems2542/src/pages/Login.jsx
+++ ems2542/src/pages/Login.jsx
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Card, Form, Button } from 'react-bootstrap';
+import ApiService from '../Utils/Headers';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await ApiService.sendUnAuthorizedRequest('login', 'POST', {
+        email: email,
+        password: password,
+      });
+      setEmail('');
+      setPassword('');
+
+      const accessToken = response.access_token;
+
+      // Store the access token in local storage
+      localStorage.setItem('access-token', accessToken);
+
+    } catch (error) {
+      console.error('Login failed: ', error.message);
+    }
+  };
+
   return (
     <Container fluid className='d-flex align-items-center justify-content-center bg-image' style={{ backgroundImage: 'url(https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp)' }}>
       <div className='mask gradient-custom-3'></div>
       <Card className='m-5' style={{ maxWidth: '400px' }}>
         <Card.Body className='px-5'>
           <h2 className="text-uppercase text-center mb-5">Login</h2>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-4">
-              <Form.Label>Username</Form.Label>
-              <Form.Control size="md" type="text" placeholder="Username" />
+              <Form.Label>Email</Form.Label>
+              <Form.Control size="md" type="text" placeholder="Email" value={email} onChange={handleEmailChange} />
             </Form.Group>
 
             <Form.Group className="mb-4">
               <Form.Label>Password</Form.Label>
-              <Form.Control size="md" type="password" placeholder="Password" />
+              <Form.Control size="md" type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
             </Form.Group>
 
             <Button className='mb-4 w-100 gradient-custom-4' size='lg' type="submit">Login</Button>
